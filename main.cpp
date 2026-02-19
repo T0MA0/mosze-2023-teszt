50ebfc85a6311f547e9082d3a4c806f9a3d78387
T0MA0 <bencetomasoczki@gmail.com>
2026-02-19 10:53:16 +0100

    Debugging and commenting the code

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];        // Wrong variable name: NELEMENTS
-    std::cout << '1-100 ertekek duplazasa'      //Missing semicolon and using "" insted of ''
-    for (int i = 0;)        //Missing the end of the loop and the increment of i 
+    int *b = new int[N_ELEMENTS];        // Change variable name to N_ELEMENTS
+    std::cout << "1-100 ertekek duplazasa\n";     //Add semicolon and change '' to ""
+    for (int i = 1;i<=N_ELEMENTS;i++)        //Correcting the loop 
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)     //endless loop
+    for (int i = 1; i <= N_ELEMENTS; i++)     //Correcting the loop
     {
-        std::cout << "Ertek:"       //Missing semicolon and the value we want to write out
+        std::cout << "Ertek: "<<b[i]<<"\n";       //Add semicolon and the value we want to write out 
     }    
-    std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;          //Wrong variable type and garbage value
-    for (int i = 0; i < N_ELEMENTS, i++)  //Using semicolon insted of simple comma
+    std::cout << "\nAtlag szamitasa: " << std::endl;
+    double atlag = 0;          //Change the type to double and initialized the value of the variable
+    for (int i = 1; i <= N_ELEMENTS; i++)  //Change simple comma to semicolon and correcting the loop
     {
-        atlag += b[i]       //Missing semicolon
+        atlag += b[i];       //Add semicolon
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
